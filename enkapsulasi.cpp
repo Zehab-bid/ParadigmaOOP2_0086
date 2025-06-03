0a24ad600c7ffbc3df93aa716514f39abcb6fee8
Zehab-bid <zehabidha152gmail.com>
2025-06-03 15:00:17 +0700

    proges 11

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -8,7 +8,18 @@ public:
     void setSaklarNo(int i, string value) {
         SaklarNo[i] = value;
     }
-    srting getsaklarNo(int i){
+    string getsaklarNo(int i){
         return SaklarNo[i];
     }
 };
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "lampu teras rumah");
+    lampuRumah.setSaklarNo(1, "lampu ruang tamu");
+    lampuRumah.setSaklarNo(2, "lampu kamar tidur");
+    lampuRumah.setSaklarNo(3, "lampu daput");    
+
+    
+}
