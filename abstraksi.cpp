9abdbf4b258236268e461c9c39e2fb44c07968a8
Zehab-bid <zehabidha152gmail.com>
2025-06-03 14:47:23 +0700

    proges 8

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 class abstraksilas {
-private
-}
+private: string x, y;
+
+public:
+    //methode untuk mengisi nilai
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+
+};
